28bfed047695681af820d20c84340f51eec6323a
fajarkarim <fajarabdulkarim@gmail.com>
2017-08-22 10:38:55 +0700

    done

--- bugtracker-react/src/components/BugItem.js
+++ bugtracker-react/src/components/BugItem.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
 
 class BugItem extends Component {
-
-  setStatusClosed(id) {
-    // let bugs = JSON.parse(localStorage.getItem('bugs')
+  constructor (props) {
+    super(props)
+    this.state = {
+      bug: 0
+    }
   }
 
   render () {
@@ -25,13 +27,75 @@ class BugItem extends Component {
           <small className="tag is-primary">{ this.props.status }</small>
         </div>
         <footer className="card-footer">
-          <a onClick={this.setStatusClosed(this.props.id)} className="is-warning card-footer-item">Close</a>
-          <a className="card-footer-item" onClick=this.deleteBug(this.props.id)>Delete</a>
+          <a className="is-warning card-footer-item" onClick={() => this.setStatusClosed(this.props.id)}>Close</a>
+          <a className="card-footer-item" onClick={() => this.deleteBug(this.props.id)}></a>
         </footer>
       </div>
         <br/>
       </div>
     )
+  }
+}
+
+const setStatusClosed = (id) => {
+  let bugs = JSON.parse(localStorage.getItem('bugs'))
+
+  let updatedBugs = bugs.map((item) => {
+    if (item.id === id)
+    item.status = 'Close'
+    return item
+  })
+
+  localStorage.setItem('bugs', JSON.stringify(updatedBugs))
+  fetchBugs()
+
+}
+
+const deleteBug = (id) => {
+  let bugs = JSON.parse(localStorage.getItem('bugs'))
+
+  let remainingBugs = bugs.filter((item) => {
+    return item.id !== id
+  })
+  localStorage.setItem('bugs', JSON.stringify(remainingBugs))
+
+  fetchBugs()
+}
+
+let fetchBugs = () => {
+  let bugs = JSON.parse(localStorage.getItem('bugs')) || []
+  let listBugsElement = document.getElementById('listBugs')
+
+  listBugsElement.innerHTML = ''
+
+  for(let i = 0; i < bugs.length; i++) {
+    let id = bugs[i].id
+    let desc = bugs[i].description
+    let severity = bugs[i].severity
+    let assignedTo = bugs[i].assignedTo
+    let status = bugs[i].status
+
+    listBugsElement.innerHTML += `<div class="card">
+      <header class="card-header">
+        <p class="card-header-title">
+        BugId: ${id}
+        </p>
+      </header>
+      <div class="card-content">
+        <div class="content">
+          ${desc}
+          <span class="tag is-info">${severity}</span>
+          <p>Assigned To: ${assignedTo}</p>
+        </div>
+        <br>
+        <small class="tag is-primary">${status}</small>
+      </div>
+      <footer class="card-footer">
+        <a onclick="setStatusClosed('${id}')" class="is-warning card-footer-item">Close</a>
+        <a class="card-footer-item" onclick="deleteBug('${id}')">Delete</a>
+      </footer>
+    </div>
+      <br>`
   }
 }
 
--- bugtracker-react/src/components/BugList.js
+++ bugtracker-react/src/components/BugList.js
@@ -1,32 +1,10 @@
 import React, { Component } from 'react'
-import BugItem from './BugItem'
 
 class BugList extends Component {
   render () {
-    var bugList = [
-        {
-          id: 1,
-          description: 'lala',
-          severity: 'easy',
-          assignedTo: 'fajar',
-          status: 'unsolved'
-        },
-        {
-          id: 2,
-          description: 'lili',
-          severity: 'medium',
-          assignedTo: 'fajar',
-          status: 'solved'
-        }
-      ]
-
     return (
-      <div>
-      {bugList.map( b => {
-        return (
-              <BugItem className="columns" id={b.id} description={b.description} severity={b.severity} assignedTo={b.assignedTo} status={b.status}/>
-            )
-        })}
+      <div className="columns">
+        <div className="column is-medium" id="listBugs"></div>
       </div>
     )
   }
